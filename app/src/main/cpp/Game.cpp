564a36921097e2bbf1ae908e734d8906c1505435
xuhua <cxuhua@gmail.com>
2019-06-05 15:38:18 +0800

    save

--- app/src/main/cpp/Game.cpp
+++ app/src/main/cpp/Game.cpp
@@ -8,6 +8,7 @@
 
 
 #include <engine/cxSprite.h>
+#include <engine/cxRotate.h>
 #include "Game.h"
 
 CX_CPP_BEGIN
@@ -32,6 +33,12 @@ void Game::OnMain()
     sp->SetTexture("item.png");
     sp->SetSize(cxSize2F(200,200));
     Window()->Append(sp);
+
+    cxRotateTo *rt = cxRotateTo::Create(M_PI*4,10);
+    rt->AttachTo(sp);
+    rt->onExit+=[](cxAction *pav){
+        pav->Reverse()->AttachTo(pav->View());
+    };
 }
 
 CX_CPP_END
